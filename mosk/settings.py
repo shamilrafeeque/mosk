6541607f60d8e60d448483c737b0f0f84c75a7f3
shamil <shamilrafeeque)4@gmail.com>
2022-08-07 23:27:39 +0530

    create storagebackends.py

--- mosk/settings.py
+++ mosk/settings.py
@@ -185,11 +185,18 @@ AWS_STORAGE_BUCKET_NAME=config('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_CUSTOM_DOMAIN='%s.s3.amazonaws.com''% moskcart'
 # AWS_S3_OBJECT_PARAMETERS={ 
 # 'CacheControl':'max-age=86400', 
+
+
+
+
+
 # } 
 # AWS_S3_FILE_OVERWRITE=True 
 # AWS_DEFAULT_ACL='public-read' 
 AWS_LOCATION='static' 
-# DEFAULT_FILE_STORAGE='storages.backends.s3boto3.S3Boto3Storage' 
+DEFAULT_FILE_STORAGE='storages.backends.s3boto3.S3Boto3Storage' 
+DEFAULT_FILE_STORAGE='mosk.storage_backends.MediaStorage' 
+
 # STATICFILES_DIRS=[ 
 #     'static', 
 # ] 
