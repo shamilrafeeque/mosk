0c418ddebb5e0fdfd425f3e90771a76444b25a84
shamil <shamilrafeeque)4@gmail.com>
2022-08-08 01:20:55 +0530

    static add

--- mosk/settings.py
+++ mosk/settings.py
@@ -135,10 +135,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static'
 STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS=[
-    'static/'
+    'static'
 ]
 # #media file configuration
 
