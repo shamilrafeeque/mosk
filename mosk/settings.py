63acb39127352eb6ee6bc900f346c8d015cae591
shamil <shamilrafeeque)4@gmail.com>
2022-08-09 13:23:04 +0530

    set email and sescription

--- mosk/settings.py
+++ mosk/settings.py
@@ -157,7 +157,8 @@ MESSAGE_TAGS = {
    
 }
 #SMTP cofiguration 
-EMAIL_BACKEND=config('EMAIL_BACKEND')
+# EMAIL_BACKEND=config('EMAIL_BACKEND')
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST=config('EMAIL_HOST')
 EMAIL_PORT=config('EMAIL_PORT',cast=int)
 EMAIL_HOST_USER=config('EMAIL_HOST_USER')
